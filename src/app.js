7db20a56f1cea3842b5ee299b5cf3fe1b22a367b
Peter Esveld <peter.esveld@gmail.com>
2017-06-17 10:33:43 -0400

    Updated with p5js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,27 @@
-console.log("hello there");
+import './app.scss'
 
-require("./app.scss");
+const sketch = (p) => {
+
+  let canvas;
+
+  p.setup = function () {
+    // We are still calling createCanvas like in the past, but now
+    // we are storing the result as a variable. This way we can
+    // call methods of the element, to set the position for instance.
+    canvas = p.createCanvas(600, 400);
+
+    // Here we call methods of each element to set the position
+    // and id, try changing these values.
+    // Use the inspector to look at the HTML generated from this
+    // code when you load the sketch in your browser.
+    canvas.parent("processing");
+  }
+
+  p.draw = function () {
+    // These commands are applied to the graphics canvas as normal.
+    p.background(220, 180, 200);
+    p.ellipse(p.width/2, p.height/2, 100, 100);
+    p.ellipse(p.width/4, p.height/2, 50, 50);
+  }
+}
+new $p5(sketch);
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
@@ -8,8 +9,25 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: './bundle.js'
   },
+  resolve: {
+    alias: {
+      //jquery: "jquery/src/jquery"
+      p5: "p5/lib/p5",
+      pDom: "p5/lib/addoms/p5.dom"
+    }
+  },
   module: {
     rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      },
       {
         test: /\.pug$/,
         use: 'pug-loader'
@@ -25,6 +43,9 @@ module.exports = {
     ]
   },
   plugins: [
+    //new webpack.ProvidePlugin({ $: 'jquery', jQuery: 'jquery', jquery: 'jquery' }),
+    new webpack.ProvidePlugin({ $p5: 'p5' }),
+    new webpack.ProvidePlugin({ $pDom: 'pDom' }),
     new HtmlWebpackPlugin({ hash: true, template: './src/index.pug', filename: 'index.html' }),
     new HtmlWebpackPlugin({ hash: true, template: './src/projects.pug', filename: 'projects.html'}),
     new HtmlWebpackPlugin({ hash: true, template: './src/contact.pug', filename: 'contact.html' }),
